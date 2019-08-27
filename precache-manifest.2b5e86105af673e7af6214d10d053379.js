e9b51d8b218b3264db44bfb878b5d8da3ceb488a
Stefano Armenes <stefano.armenes@gmail.com>
2019-08-27 15:54:26 +0200

    Updates

---
+++ precache-manifest.2b5e86105af673e7af6214d10d053379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffbd95fb032998914f4f3140934d9f1",
+    "url": "/social-ping/index.html"
+  },
+  {
+    "revision": "ef10a475a447e3e3602e",
+    "url": "/social-ping/static/css/main.fcdf413f.chunk.css"
+  },
+  {
+    "revision": "b53e1483c2195bd8a4fc",
+    "url": "/social-ping/static/js/2.796594fc.chunk.js"
+  },
+  {
+    "revision": "ef10a475a447e3e3602e",
+    "url": "/social-ping/static/js/main.ea186bef.chunk.js"
+  },
+  {
+    "revision": "94f0b1f14ea0c12e4e87",
+    "url": "/social-ping/static/js/runtime~main.25b90d7b.js"
+  }
+]);
